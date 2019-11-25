3b3940fd4a188f331316e0786c50e0b07d8a8338
caotin <caotin15cntt@gmail.com>
2019-11-25 21:18:52 +0700

    add ant design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,14 +4,15 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { LayoutsModule } from 'src/app/layouts/layouts.module';
-
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    LayoutsModule
+    LayoutsModule,
+    NzButtonModule
   ]
 })
 export class HomeModule { }
